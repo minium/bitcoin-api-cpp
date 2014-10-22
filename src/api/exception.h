6761ea3c53c4007ea2740c40d7cd651b2d0a889b
Krzysztof Okupski <k.okupski@gmail.com>
2014-10-22 16:31:34 +0200

    Renamed files

---
+++ src/api/exception.h
@@ -0,0 +1,59 @@
+/**
+ * exception.h
+ * 
+ * Declaration of error class for the JSON-RPC wrapper.
+ *
+ * @author Krzysztof Okupski
+ * @version 1.0
+ */
+
+#ifndef BITCOINERROR_H
+#define BITCOINERROR_H
+
+#include <string>
+#include <sstream>
+
+class BitcoinException: public std::exception
+{
+private:
+	int code;
+	std::string msg;
+
+public:
+	explicit BitcoinException(int errcode, const std::string& message) {
+		this->code = errcode;
+		this->msg = parse(message);
+
+	}
+	~BitcoinException() throw() { };
+
+	int getCode(){
+		return code;
+	}
+
+	std::string getMessage(){
+		return msg;
+	}
+
+	std::string parse(const std::string& in){
+		std::string out = in;
+		std::string pattern = ": ";
+		unsigned int pos = out.find(pattern);
+		if(pos <= out.size()){
+			out.erase(pos, pattern.size());
+			out[0] = toupper(out[0]);
+		}
+
+		return out;
+	}
+
+	virtual const char* what() const throw (){
+		std::stringstream out;
+		out << "Error " << code << ": " << msg;
+		return out.str().c_str();
+	}
+};
+
+
+
+#endif
